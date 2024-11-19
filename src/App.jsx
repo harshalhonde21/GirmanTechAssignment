289210df3887f21fb10c49d8821521d54ee580f3
Harshal Honde <105191619+harshalhonde21@users.noreply.github.com>
2024-11-19 21:32:14 +0530

    updated with buld error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
-import Home from "./pages/Home";
+import Home from "./Pages/Home";
 import Detail from "./Pages/Detail";
 import Contact from "./Pages/Contact";
 
